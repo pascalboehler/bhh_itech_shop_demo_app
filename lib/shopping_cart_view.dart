dedb09de10a51b3cc38c68156750c7bc33f3b69f
Pascal Böhler <pascal.boehler@hotmail.de>
2023-01-22 13:50:00 +0100

    feat: Added MaterialRoute to home

--- lib/shopping_cart_view.dart
+++ lib/shopping_cart_view.dart
@@ -5,6 +5,7 @@ import 'helper view/main_nav_drawer.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'styles/styles.dart';
+import 'home_screen.dart';
 
 class ShoppingCartView extends StatelessWidget {
   @override
@@ -43,17 +44,18 @@ class ShoppingCartView extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("It's so empty here:(", style: TextStyle(fontSize: 35.0),),
-          Icon(
+          const Text("It's so empty here:(", style: TextStyle(fontSize: 35.0),),
+          const Icon(
             Icons.shopping_bag_outlined,
             size: 400.0,
             color: Styles.standardColor,
           ),
           
           TextButton(onPressed: () {
-            print("Hi");
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => HomeScreen()));
           }, 
-          child: Text("Go to home", style: TextStyle(fontSize: 35.0),))
+          child: const Text("Go to home", style: TextStyle(fontSize: 35.0),))
         ],
       ),
     );
